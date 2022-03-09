57ad90112e07c6e9d27455ec612f77896c947f23
RyanDeLong <rdelong111@gmail.com>
2022-03-09 12:56:58 -0600

    commit tree 481a5b5f3f51fd952b82925912f2824bc7728803

--- javascript/rotate_array.js
+++ javascript/rotate_array.js
@@ -1,5 +1,14 @@
 function rotateArray(arr, k) {
-  // type your code here
+  if (k >= arr.length) {
+    k = k % arr.length;
+  }
+
+  if (k !== 0) {
+    return arr.splice(-k).concat(arr);
+  }
+  else {
+    return arr;
+  }
 }
 
 if (require.main === module) {
@@ -20,5 +29,18 @@ if (require.main === module) {
 
 module.exports = rotateArray;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/*
+If k is greater than or equal to the length of arr
+  k is equal to the remainder of dividing k by the length of arr
+
+If k is now not equal to 0
+  return an array with the combination of an array of the last 'k' elements and altered arr
+If k is equal to 0
+  return arr
+*/
+
+/*
+The value k is altered and possibly reduced because if k is greater than the size of arr, then rotations are just being repeated. The new k is the remainder of k divided by the length of arr.
+If k is equal to zero, then the array will look like it hasn't been rotated so arr is returned.
+If k does not equal to zero, then arr will rotate k times.
+*/
